68ba01b9657dbb5919b470718abed35e589a2c5d
dawson102719-jpg <dawson102719@gmail.com>
2026-07-17 15:06:19 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//when this function is called the page turs red
+function background() {
+	document.body.style.backgroundColor = "red";
+
+}
+
+//when this funciton is called, the page turns green
+function background1(){
+	document.body.style.backgroundColor = "green";
+}
